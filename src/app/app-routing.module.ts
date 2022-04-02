727a497143b99216b941e6743ff92f4c02c8ca80
IrishBruse <econn50@outlook.com>
2022-04-03 00:35:27 +0100

    Tidy up pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,38 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'tab1',
-    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
-  },
-  {
-    path: 'tab2',
-    loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
-  },
-  {
-    path: 'tab3',
-    loadChildren: () => import('./tab3/tab3.module').then( m => m.Tab3PageModule)
-  },
-  {
-    path: 'tab4',
-    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
-  }
-
+    {
+        path: "",
+        loadChildren: () =>
+            import("./tabs/tabs.module").then((m) => m.TabsPageModule),
+    },
+    {
+        path: "search",
+        loadChildren: () =>
+            import("./pages/search/search.module").then(
+                (m) => m.SearchPageModule
+            ),
+    },
+    {
+        path: "watching",
+        loadChildren: () =>
+            import("./pages/watching/watching.module").then(
+                (m) => m.WatchingPageModule
+            ),
+    },
+    {
+        path: "upcoming",
+        loadChildren: () =>
+            import("./pages/upcoming/upcoming.module").then(
+                (m) => m.UpcomingPageModule
+            ),
+    },
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    ],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
---
+++ src/app/pages/watching/watching.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-watching",
+    templateUrl: "./watching.page.html",
+    styleUrls: ["./watching.page.scss"],
+})
+export class WatchingPage implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
